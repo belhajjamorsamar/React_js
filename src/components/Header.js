cdd5b69e9d69c47aa2c565350f5ebd4c52a70173
belhajjamorsamar <belhajjamorsamar@gmail.com>
2023-01-24 04:29:57 +0100

    add objects

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,34 @@
 import React from 'react';
 function Header(){
+  let title ="";
+  const client="Designer";
+/*
+  if(client=== "Designer"){
+    title="Design is my Life"
+  }
+    else{
+      title="Programming is my Life"
+
+    }*/ 
+
+
+   const Client="Designer";
+   const Title = {
+    designer :"Design",
+    programmer:"Programming"
+   }
+   const info = {
+    name:"Samar",
+    nick:"Belhajj amor"
+   }
+
+  
   return(
-    <div className='navbar bg-dark rounded'>
-         <h1 className="text-white"> Design is my life </h1>
+    <div className='navbar bg-dark rounded text-white'>
+         Hello {`${info.name} ${info.nick}`}
+         <h2 > {Client == "Designer" ? Title.designer:Title.programmer } is my Life.
+         </h2>
+
       </div>
     );
 }
