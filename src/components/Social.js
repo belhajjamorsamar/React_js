8c23de2e64cb4f11ed57b532ae9d864efe48e65d
belhajjamorsamar <belhajjamorsamar@gmail.com>
2023-01-29 23:33:01 +0100

    props

--- src/components/Social.js
+++ src/components/Social.js
@@ -5,9 +5,10 @@ function Social(){
     return(
     <div>
        <ul className="list-group">
-         <SocialItem  media='facebook' id="facbook" img="https://picsum.photos/101"/>
-         <SocialItem  media='Youtube' id="youtube"  img="https://picsum.photos/100" />
-         <SocialItem  media='Instgramme' id="instgramme" img="https://picsum.photos/102"/>
+         <SocialItem  media='facebook'   id="facbook"   img="https://picsum.photos/101" alt="facebook"/>
+         <SocialItem  media='Youtube'    id="youtube"   img="https://picsum.photos/100" alt="youtube"/>
+         <SocialItem  media='Instgramme' id="instgramme"img="https://picsum.photos/102" alt="intsgramme"/>
+         <SocialItem  media='Linkden'    id="linkden"   img="https://picsum.photos/103" alt="linkden" />
        </ul>
 
     </div>
--- src/components/SocialItem.js
+++ src/components/SocialItem.js
@@ -3,9 +3,8 @@ function SocialItem (props){
 
     return(
         <li className="list-group-item"><input type="checkbox" value="" id= {props.id}/>
-        <img src={props.img}/>
-
-           <label  for= {props.id}>
+        <img src={props.img} alt={`icons of ${props.alt}`}/>
+           <label  htmlfor= {props.id}>
            {props.media}
            </label>
        </li>
