96fe39ab187a16f492ff2b2c5ad49193f4109f33
belhajjamorsamar <belhajjamorsamar@gmail.com>
2023-04-20 05:34:33 +0200

    des composants dans des composant

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,16 @@ import Header from './Header'
 import Team from './Team'
 
 
+function MonCompsant(){
+return (
+  <div>
+<h1> mon nom est BELHAJJAMOR </h1>
+<h2>mon prénom est Samar</h2>
+  </div>
+);
+
+}
+
 function App(){
   function apple(){
     alert('Hello from apple')
@@ -11,6 +21,7 @@ function App(){
     <div className='container'>
       <h2>Hello i'm Heare</h2>
    <button onClick={apple}>click md</button>
+   <MonCompsant/>
     </div>
   );}
 export default App;
