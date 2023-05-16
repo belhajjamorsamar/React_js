b3847597060fdb3670c2b0011ac808eae438e19f
belhajjamorsamar <belhajjamorsamar@gmail.com>
2023-05-16 05:42:39 +0200

    State sans Construtor

--- src/components/LearnComponents/Horloge.js
+++ src/components/LearnComponents/Horloge.js
@@ -2,11 +2,16 @@ import Recat , {Component}from 'react'
 
 class Horloge extends Component{
 
-    constructor(props){
+    /*constructor(props){
         super(props);
         this.state={date :new Date()};
-    }
+    }*/
 
+    //******************ou bien ********************** */
+    state=
+    {
+         date:new Date(), //tu peut ajouter des autre actions 
+    }
     render(){
         return(
             <div>Horloge :{this.state.date.toLocaleTimeString()}</div>
