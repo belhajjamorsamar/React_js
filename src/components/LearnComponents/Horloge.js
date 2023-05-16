8b0d8a868111db999725ac1172d00cbfa6d3a45b
belhajjamorsamar <belhajjamorsamar@gmail.com>
2023-05-16 23:08:55 +0200

    ComponenetdidMount

--- src/components/LearnComponents/Horloge.js
+++ src/components/LearnComponents/Horloge.js
@@ -12,6 +12,9 @@ class Horloge extends Component{
     {
          date:new Date(), //tu peut ajouter des autre actions 
     }
+componentDidMount(){
+    setInterval(()=>this.setState({date:new Date()}),1000); // chaque seconde il va afficher tick dans lo console
+}
     render(){
         return(
             <div>Horloge :{this.state.date.toLocaleTimeString()}</div>
