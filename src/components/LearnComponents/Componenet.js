9cdbbd9c2d8f79343f0a640a2b13e43b6c3ecf2a
belhajjamorsamar <belhajjamorsamar@gmail.com>
2023-05-16 05:02:57 +0200

    Componenet Class

--- src/components/LearnComponents/Componenet.js
+++ src/components/LearnComponents/Componenet.js
@@ -6,12 +6,44 @@ import React from 'react';
 
 // 1 : function
 
-function Componenet(){
+/*function Componenet(){
     return(
         <h1>Hii I'm a function Componenet</h1>
 
     );
 
-}
+}*/
+
+
+// 2: Class : <div></div> 
+ /*class Componenet  extends React.Component{
+    render(){
+
+        return(
+            <div>
+                <h1>Hi i'm a Class Componenet</h1>
+            </div>
+        );
+    }
+
+
+ } */
+
+
+ 
+// 2: Class : []  : utiliser un tablaux pour  retourner plusieurs element
+class Componenet  extends React.Component{
+    render(){
+
+        return(
+            [
+                <h1>Hi i'm a Class Componenet</h1>,
+                <h2> Enclosed by table</h2>
+            ]
+        );
+    }
+
+
+ } 
 
 export default Componenet;
