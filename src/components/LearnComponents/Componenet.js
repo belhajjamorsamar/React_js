c0b6a7952eb4ae66659a14fb06d810aa5b3ddafd
belhajjamorsamar <belhajjamorsamar@gmail.com>
2023-05-16 05:08:01 +0200

    entouré par <></>

--- src/components/LearnComponents/Componenet.js
+++ src/components/LearnComponents/Componenet.js
@@ -48,18 +48,34 @@ import React, { Fragment } from 'react';
 
 
  // 2: Class : <Fragment></Fragment> : utiliser un tablaux pour  retourner plusieurs element
-class Componenet  extends React.Component{
+/*class Componenet  extends React.Component{
     render(){
 
         return(
             <Fragment>
                 <h1>Hi i'm a Class Componenet</h1>,
-                <h2> Enclosed by table</h2>
+                <h2> Enclosed by Fragment</h2>
             </Fragment>
         );
     }
 
 
+ } */
+
+ // 2: Class : <></> : utiliser un ces balize  
+class Componenet  extends React.Component{
+    render(){
+
+        return(
+            <>
+                <h1>Hi i'm a Class Componenet</h1>,
+                <h2> Enclosed by "<></>" </h2>
+            </>
+        );
+    }
+
+
  } 
+
 
 export default Componenet;
